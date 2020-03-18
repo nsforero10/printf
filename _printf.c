a0241ca1385e1f5849ebbafdbafa0f72973801bb
Nico Forero <ns.forero10@gmail.com>
2020-03-17 22:01:32 -0500

    [Feature] Adding almost whole conversions

--- _printf.c
+++ _printf.c
@@ -3,19 +3,6 @@
 #include <unistd.h>
 
 /**
- * str_ln - calculates the lenght of a string
- * @p: the string to messure
- * Return: the lenght of the string
- */
-int str_ln(char *p)
-{
-	int i = 0;
-
-	while (*p)
-		i++, p++;
-	return (i);
-}
-/**
  * get_fmt_funct - get the apropiatted function to handle
  * the the conversion
  * @s: the identifier to handle
@@ -26,7 +13,18 @@ char * (*get_fmt_funct(const char *s))(va_list, char *)
 	unsigned int i = 0;
 	format_t fmts[] = {
 		{'c', handle_print_char},
-		{'s', handle_print_string}
+		{'s', handle_print_string},
+		{'d', handle_print_int},
+		{'i', handle_print_int},
+		{'u', handle_print_uint},
+		{'b', handle_print_binary},
+		{'o', handle_print_octal},
+		{'x', handle_print_hexa},
+		{'X', handle_print_upper_hexa},
+		{'p', handle_print_pointer},
+		{'r', handle_print_reversed_string},
+		{'S', handle_print_custom_string},
+		{'R', handle_print_rot13_string}
 	};
 
 	while (fmts[i].identifier)
@@ -45,32 +43,40 @@ char * (*get_fmt_funct(const char *s))(va_list, char *)
  */
 int _printf(const char *format, ...)
 {
-	char *buff = malloc(sizeof(char) * 2048), *org, *aux = NULL;
+	char *buff = calloc(2048, 1), *cursor = buff;
+	char *(*funct)(va_list, char *) = NULL;
 	va_list args;
-	unsigned int size = 0, org_s = 0;
 
 	if (!buff)
-	{
-		free(buff);
-		return (0);
-	}
-	org = buff;
+
+		return (free(buff), 1);
+	if (!format)
+		return (-1);
 	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			org_s = str_ln(org);
-			aux = get_fmt_funct(++format)(args, buff);
-			if (aux)
-				size += str_ln(org) - org_s, buff = aux;
+			if (buff == cursor && !*(format  + 1))
+				return (-1);
+			funct = get_fmt_funct(format + 1);
+			if (funct)
+				cursor = funct(args, cursor), format++;
 			else
-				exit(98);
+			{
+				if (*(format  + 1) == '%')
+					format++;
+				*cursor = *format, cursor++;
+			}
 		}
 		else
-			*buff = *format, buff++,  size++;
+			*cursor = *format, cursor++;
 		format++;
 	}
-	va_end(args), write(1, org, size), free(org);
-	return (size);
+	va_end(args);
+	*cursor = '\0', write(1, buff, str_ln(buff));
+	if (*buff == '\0')
+		return (free(buff), 1);
+	free(buff);
+	return (str_ln(buff));
 }
--- print_functs.c
+++ print_functs.c
@@ -5,31 +5,98 @@
 #include <unistd.h>
 
 /**
- * handle_print_char - handles fill the buffer with chars
+ * handle_print_char - handles fill the cursor with chars
  * @list: - va_list to extract the data
- * @buffer: - the buffer to fill
- * Return: the buffer filled
+ * @cursor: - the cursor to fill
+ * Return: the cursor filled
  */
-char *handle_print_char(va_list list, char *buffer)
+char *handle_print_char(va_list list, char *cursor)
 {
-	char current = va_arg(list, int);
+	char current = va_arg(list, unsigned int);
 
-	*buffer = current;
-	return (++buffer);
+	if (current)
+		*cursor = current, cursor++;
+	return (cursor);
 }
 
 /**
- * handle_print_string - handles fill the buffer with chars
+ * handle_print_string - handles fill the cursor with chars
  * @list: - va_list to extract the data
- * @buffer: - the buffer to fill
- * Return: the buffer filled
+ * @cursor: - the cursor to fill
+ * Return: the cursor filled
  */
-char *handle_print_string(va_list list, char *buffer)
+char *handle_print_string(va_list list, char *cursor)
 {
 	char *current;
 
 	current = va_arg(list, char *);
+	if (!current)
+		current = "(null)";
 	while (*current)
-		*buffer = *current, buffer++, current++;
-	return (buffer);
+		*cursor = *current, cursor++, current++;
+	return (cursor);
 }
+
+/**
+ * handle_print_uint - handles fill the cursor with chars
+ * @list: - va_list to extract the data
+ * @cursor: - the cursor to fill
+ * Return: the cursor filled
+ */
+char *handle_print_uint(va_list list, char *cursor)
+{
+	unsigned int current = va_arg(list, unsigned int);
+
+	if (current == 0)
+	{
+		*cursor = '0', cursor++;
+		return (cursor);
+	}
+	if (current)
+		cursor = converToStrBase(current, 10, cursor);
+	return (cursor);
+}
+/**
+ * handle_print_int - handles fill the cursor with chars
+ * @list: - va_list to extract the data
+ * @cursor: - the cursor to fill
+ * Return: the cursor filled
+ */
+char *handle_print_int(va_list list, char *cursor)
+{
+	int current = va_arg(list, int);
+
+	if (current == 0)
+	{
+		*cursor = '0', cursor++;
+		return (cursor);
+	}
+	if (current)
+	{
+		if (current < 0)
+			*cursor = '-', current = current * -1, cursor++;
+		cursor = converToStrBase(current, 10, cursor);
+	}
+	return (cursor);
+}
+
+/**
+ * handle_print_binary - handles fill the cursor with chars
+ * @list: - va_list to extract the data
+ * @cursor: - the cursor to fill
+ * Return: the cursor filled
+ */
+char *handle_print_binary(va_list list, char *cursor)
+{
+	unsigned int current = va_arg(list, unsigned int);
+
+	if (current == 0)
+	{
+		*cursor = '0', cursor++;
+		return (cursor);
+	}
+	if (current)
+		cursor = converToStrBase(current, 2, cursor);
+	return (cursor);
+}
+
